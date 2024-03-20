98a7ee1abb8d085d0a2475bc50f64d226c229cba
Kazu-kun <kasundodanwala@gmail.com>
2024-03-20 10:47:20 +0530

    seggs

--- Practicals/Flutter/nekoflixx/lib/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Colours {
-  static const scaffoldBgColor = Color.fromARGB(255, 25, 0, 0);
-  static const ratingColor = Color.fromARGB(255, 172, 245, 0);
-}
--- Practicals/Flutter/project_003_firebase_login/android/app/src/main/kotlin/com/example/project_003_firebase_login/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.project_003_firebase_login
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
